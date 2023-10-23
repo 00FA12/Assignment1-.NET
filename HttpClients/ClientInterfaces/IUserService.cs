9815aeb8ed40e6a544e862bad8754f3e20f18c4e
00FA12 <114292738+00FA12@users.noreply.github.com>
2023-10-23 13:13:30 +0200

    Blazor configured + Create Users

---
+++ HttpClients/ClientInterfaces/IUserService.cs
@@ -0,0 +1,9 @@
+using Domain.DTOs;
+using Domain.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IUserService
+{
+    Task<User> Create(UserCreationDTO dto);
+}
---
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -0,0 +1,35 @@
+using System.Net.Http.Json;
+using System.Text.Json;
+using Domain.DTOs;
+using Domain.Models;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.Implementations;
+
+public class UserHttpClient : IUserService
+{
+    private readonly HttpClient _client;
+
+    public UserHttpClient(HttpClient _client)
+    {
+        this._client = _client;
+    }
+
+
+    public async Task<User> Create(UserCreationDTO dto)
+    {
+        HttpResponseMessage response = await _client.PostAsJsonAsync("/users", dto);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return user;
+
+    }
+}
